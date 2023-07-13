ef051bdb7a56efd8d759b078b02797b703e8d704
Amanda de Oliveira Spitzner <95360085+amandaspitz@users.noreply.github.com>
2023-07-13 18:06:29 -0300

    Add files via upload

---
+++ src/EntryPoint.java
@@ -0,0 +1,7 @@
+//Importe sua classe aqui
+
+class EntryPoint {
+    public static void main(String[] args) {
+        //Instancie sua classe aqui        
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,31 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String numeroTelefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public void setNumeroTelefone(String numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+}
+
+export JAVA_HOME=C:\Program Files\Java\jdk-11
